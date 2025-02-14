04624d68d678088ff01851a5b244f11b69d62e93
Paulo Cesar Carneiro <paulo_c_carneiro@hotmail.com>
2025-02-13 22:26:07 -0300

    versão 1 do relatorio de despesas x orçamento.

---
+++ src/PCF/PCF.SPA/Pages/Reports/RelatorioOrcamento.razor.cs
@@ -0,0 +1,37 @@
+﻿using PCF.SPA.Services;
+using PCF.SPA.Components.Orcamento;
+using Microsoft.AspNetCore.Components;
+namespace PCF.SPA.Pages.Reports
+{
+    public partial class RelatorioOrcamento
+    {
+        private IEnumerable<OrcamentoResponseViewModel> _orcamentos = new List<OrcamentoResponseViewModel>(); // Initialize the list
+        private bool _loading = true;
+
+        [Inject] private IWebApiClient WebApiClient { get; set; } // Correct the inject syntax
+        [Inject] private IDialogService DialogService { get; set; } // Correct the inject syntax
+        [Inject] private ISnackbar Snackbar { get; set; } // Correct the inject syntax
+
+        protected override async Task OnInitializedAsync()
+        {
+            await LoadOrcamentosAsync();
+        }
+
+        private async Task LoadOrcamentosAsync()
+        {
+            _loading = true;
+            try
+            {
+                _orcamentos = await WebApiClient.OrcamentosAllAsync();
+            }
+            catch (Exception ex)
+            {
+                Snackbar.Add($"Erro não esperado: {ex.Message}", Severity.Error);
+            }
+            finally
+            {
+                _loading = false;
+            }
+        }
+    }
+}
